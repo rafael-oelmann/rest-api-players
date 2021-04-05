1e21f3aeb3fb6c4f66ac7e072d8acef36c71c187
Rafael <rafael.oelmann@gmail.com>
2021-04-05 16:18:33 +0100

    added player schema

---
+++ models/Player.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const PlayerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gamesPlayed: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  wins: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  losses: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  draws: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  goals: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  assits: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("Player", PlayerSchema);
--- routes/players.js
+++ routes/players.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const router = express.Router();
+const Player = require("../models/Player");
 
 router.get("/", (req, res) => {
   res.send("We are on players");
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
