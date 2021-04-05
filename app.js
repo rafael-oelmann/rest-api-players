412ac0fa7105ddf49985fcbd0f034729af36f899
Rafael <rafael.oelmann@gmail.com>
2021-04-05 15:26:05 +0100

    added mongoose connection

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+//Middlewares
 
 //ROUTES
 app.get("/", (req, res) => {
   res.send("We are on home");
+});
+
+//Connect To DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("Connected to DB!");
 });
 
 app.listen(3000);
